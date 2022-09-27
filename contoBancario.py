c38b3873d403a06da80e740acaeee5e543cec276
Cavacmd <72693219+Cava-cmd@users.noreply.github.com>
2022-09-27 09:48:07 +0000

    removed unused libs

--- contoBancario.py
+++ contoBancario.py
@@ -1,1 +1,13 @@
 from persona import Persona
+
+class ContoBancario(Persona):
+    def __init__(self, nome: str, cognome: str, conto):
+        super().__init__(nome, cognome)
+        self.conto = conto
+        self.saldo = int(0)
+
+    def getSaldo(self):
+        return self.saldo
+
+    def versa(self):
+        
