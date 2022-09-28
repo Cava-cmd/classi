06560c962969efd11dc252253fc93a735580da6d
Yami <>
2022-09-28 16:53:11 +0000

    Completata la classe Persona.py

--- Persona.py
+++ Persona.py
@@ -1,5 +1,10 @@
 class Persona:
     def __init__(self, nome: str, cognome: str):
+      
         self.nome = nome
         self.cognome = cognome
 
+    def getNome(self): 
+        return set.nome
+    def getCognome(self): 
+        return set.cognome
